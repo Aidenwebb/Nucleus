c66e2337a7a76cc30aa12649655990f2bd8a5c24
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2024-01-09 18:02:36 +0000

    Add Migrations

---
+++ src/Infrastructure/Data/Migrations/20240109175820_Add Ticket Priority Calculation.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nucleus.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTicketPriorityCalculation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "Tickets",
+                type: "int",
+                nullable: false,
+                computedColumnSql: "[Impact] * [Urgency]",
+                stored: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Tickets");
+        }
+    }
+}
