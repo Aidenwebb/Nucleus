87b1022fcd35b637ff96ada1f97abcf7a42a2cac
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2024-01-09 17:25:03 +0000

    Create Ticket Model MVP

---
+++ src/Domain/TicketAggregate/Entities/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using Nucleus.Domain.CompanyAggregate.Entities;
+using Nucleus.Domain.ContactAggregate.Entities;
+using Nucleus.Domain.TicketAggregate.Enums;
+
+namespace Nucleus.Domain.TicketAggregate.Entities;
+
+public class Ticket : BaseAuditableEntity
+{
+    public string? Summary { get; set; }
+    public string? Description { get; set; }
+    public string? ExternalReference { get; set; }
+
+    public int? CompanyId { get; set; }
+    public Company Company { get; set; } = null!;
+    
+    public int? ContactId { get; set; }
+    public Contact Contact { get; set; } = null!;
+
+    public TicketUrgency Urgency { get; set; } = TicketUrgency.Medium;
+    public TicketImpact Impact { get; set; } = TicketImpact.Medium;
+}
---
+++ src/Domain/TicketAggregate/Events/TicketCreatedEvent.cs
@@ -0,0 +1,13 @@
+﻿using Nucleus.Domain.TicketAggregate.Entities;
+
+namespace Nucleus.Domain.TicketAggregate.Events;
+
+public class TicketCreatedEvent : BaseEvent
+{
+    public Ticket Ticket { get; }
+
+    public TicketCreatedEvent(Ticket ticket)
+    {
+        Ticket = ticket;
+    }
+}
