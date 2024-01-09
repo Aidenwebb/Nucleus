38e26fc37c0f459045423bb0a1cc606c6cc6736f
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2024-01-09 17:34:23 +0000

    Add Ticket MVP Migrations

--- src/Infrastructure/Data/Migrations/20240109173305_Add Ticket MVP.Designer.cs
+++ src/Infrastructure/Data/Migrations/20240109173305_Add Ticket MVP.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nucleus.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using Nucleus.Infrastructure.Data;
 namespace Nucleus.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240109173305_Add Ticket MVP")]
+    partial class AddTicketMVP
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
