c3aed1a49f3242fe9a8c685841aee1dd58794adc
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2024-01-09 12:01:53 +0000

    Add Update and Delete company

---
+++ src/Application/Companies/Commands/DeleteCompany/DeleteCompany.cs
@@ -0,0 +1,37 @@
+﻿using Nucleus.Application.Common.Interfaces;
+using Nucleus.Domain.CompanyAggregate.Events;
+
+namespace Nucleus.Application.Companies.Commands.DeleteCompany;
+
+public record DeleteCompanyCommand(int Id) : IRequest;
+
+public class DeleteCompanyCommandValidator : AbstractValidator<DeleteCompanyCommand>
+{
+    public DeleteCompanyCommandValidator()
+    {
+    }
+}
+
+public class DeleteCompanyCommandHandler : IRequestHandler<DeleteCompanyCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteCompanyCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(DeleteCompanyCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.Companies
+            .FindAsync(new object[] { request.Id }, cancellationToken);
+
+        Guard.Against.NotFound(request.Id, entity);
+
+        _context.Companies.Remove(entity);
+        
+        entity.AddDomainEvent(new CompanyDeletedEvent(entity));
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
--- src/Web/Endpoints/Company/Companies.cs
+++ src/Web/Endpoints/Company/Companies.cs
@@ -1,5 +1,7 @@
 ﻿using Nucleus.Application.Common.Models;
 using Nucleus.Application.Companies.Commands.CreateCompany;
+using Nucleus.Application.Companies.Commands.DeleteCompany;
+using Nucleus.Application.Companies.Commands.UpdateCompany;
 using Nucleus.Application.Companies.Queries.GetCompaniesWithPagination;
 using Nucleus.Application.Companies.Queries.GetCompany;
 
@@ -13,11 +15,10 @@ public class Companies : EndpointGroupBase
             .RequireAuthorization()
             .MapPost(CreateCompany)
             .MapGet(GetCompanyById, "{id}")
-            .MapGet(GetCompaniesWithPagination);
-
-        // .MapPut(UpdateCompany, "{id}")
+            .MapGet(GetCompaniesWithPagination)
+            .MapPut(UpdateCompany, "{id}")
         // .MapPut(UpdateCompanyDetail, "UpdateDetail/{id}")
-        // .MapDelete(DeleteCompany, "{id}");
+            .MapDelete(DeleteCompany, "{id}");
     }
     
     public async Task<int> CreateCompany(ISender sender, CreateCompanyCommand command)
@@ -34,26 +35,26 @@ public class Companies : EndpointGroupBase
     {
         return await sender.Send(query);
     }
-    //
-    //
-    //
-    // public async Task<IResult> UpdateCompany(ISender sender, int id, UpdateCompanyCommand command)
-    // {
-    //     if (id != command.Id) return Results.BadRequest();
-    //     await sender.Send(command);
-    //     return Results.NoContent();
-    // }
-    //
+    
+    
+    
+    public async Task<IResult> UpdateCompany(ISender sender, int id, UpdateCompanyCommand command)
+    {
+        if (id != command.Id) return Results.BadRequest();
+        await sender.Send(command);
+        return Results.NoContent();
+    }
+    
     // public async Task<IResult> UpdateCompanyDetail(ISender sender, int id, UpdateCompanyDetailCommand command)
     // {
     //     if (id != command.Id) return Results.BadRequest();
     //     await sender.Send(command);
     //     return Results.NoContent();
     // }
-    //
-    // public async Task<IResult> DeleteCompany(ISender sender, int id)
-    // {
-    //     await sender.Send(new DeleteCompanyCommand(id));
-    //     return Results.NoContent();
-    // }
+    
+    public async Task<IResult> DeleteCompany(ISender sender, int id)
+    {
+        await sender.Send(new DeleteCompanyCommand(id));
+        return Results.NoContent();
+    }
 }
