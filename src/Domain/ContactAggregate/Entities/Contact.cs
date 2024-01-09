d0bb83d6a65f2aeccf5a4631d8bcfada0f6ce4d8
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2024-01-09 15:23:56 +0000

    Add MVP Entity and Created / Deleted events

---
+++ src/Domain/ContactAggregate/Entities/Contact.cs
@@ -0,0 +1,18 @@
+﻿using Nucleus.Domain.CompanyAggregate.Entities;
+
+namespace Nucleus.Domain.ContactAggregate.Entities;
+
+public class Contact : BaseAuditableEntity
+{
+    public int CompanyId { get; set; }
+    public Company Company { get; set; } = null!;
+
+    public string? GivenName { get; set; }
+    public string? FamilyName { get; set; }
+
+    public int? ManagerContactId { get; set; }
+    public Contact? ManagerContact { get; set; } = null!;
+
+    public int? AssistantContactId { get; set; }
+    public Contact? AssistantContact { get; set; } = null!;
+}
---
+++ src/Domain/ContactAggregate/Events/ContactCreatedEvent.cs
@@ -0,0 +1,13 @@
+﻿using Nucleus.Domain.ContactAggregate.Entities;
+
+namespace Nucleus.Domain.ContactAggregate.Events;
+
+public class ContactCreatedEvent : BaseEvent
+{
+    public ContactCreatedEvent(Contact contact)
+    {
+        Contact = contact;
+    }
+
+    public Contact Contact { get; }
+}
