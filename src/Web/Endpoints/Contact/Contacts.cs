29ff4e8b1cd0366c8426b34bc3844e08b1371194
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2024-01-09 15:55:06 +0000

    Add Create Contact

---
+++ src/Web/Endpoints/Contact/Contacts.cs
@@ -0,0 +1,24 @@
+﻿using Nucleus.Application.Contacts.Commands.CreateContact;
+
+namespace Nucleus.Web.Endpoints.Contact;
+
+public class Contacts : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .RequireAuthorization()
+            .MapPost(CreateContact);
+            // .MapGet(GetContactById, "{id}")
+            // .MapGet(GetContactsWithPagination)
+            // .MapPut(UpdateContact, "{id}")
+            // .MapPut(UpdateContactDetail, "UpdateDetail/{id}")
+            // .MapDelete(DeleteContact, "{id}");
+    }
+    
+    public async Task<int> CreateContact(ISender sender, CreateContactCommand command)
+    {
+        return await sender.Send(command);
+    }
+    
+}
