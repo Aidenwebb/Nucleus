2eda591616fe48228500938e42312e3de9c70ec8
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2024-01-09 11:34:24 +0000

    Add Create Company

--- src/Web/Endpoints/Company/Companies.cs
+++ src/Web/Endpoints/Company/Companies.cs
@@ -1,9 +1,51 @@
-﻿// namespace Nucleus.Web.Endpoints.Company;
-//
-// public class Companies : EndpointGroupBase
-// {
-//     public override void Map(WebApplication app)
-//     {
-//         throw new NotImplementedException();
-//     }
-// }
+﻿using Nucleus.Application.Common.Models;
+using Nucleus.Application.Companies.Commands.CreateCompany;
+
+namespace Nucleus.Web.Endpoints.Company;
+
+public class Companies : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .RequireAuthorization()
+            .MapPost(CreateCompany);
+            // .MapGet(GetCompaniesWithPagination)
+            // .MapGet(GetCompanyById, "{id}")
+            // .MapPut(UpdateCompany, "{id}")
+            // .MapPut(UpdateCompanyDetail, "UpdateDetail/{id}")
+            // .MapDelete(DeleteCompany, "{id}");
+    }
+    
+    public async Task<int> CreateCompany(ISender sender, CreateCompanyCommand command)
+    {
+        return await sender.Send(command);
+    }
+    
+    // public async Task<PaginatedList<CompanyBriefDto>> GetCompaniesWithPagination(ISender sender, [AsParameters] GetCompaniesWithPaginationQuery query)
+    // {
+    //     return await sender.Send(query);
+    // }
+    //
+    //
+    //
+    // public async Task<IResult> UpdateCompany(ISender sender, int id, UpdateCompanyCommand command)
+    // {
+    //     if (id != command.Id) return Results.BadRequest();
+    //     await sender.Send(command);
+    //     return Results.NoContent();
+    // }
+    //
+    // public async Task<IResult> UpdateCompanyDetail(ISender sender, int id, UpdateCompanyDetailCommand command)
+    // {
+    //     if (id != command.Id) return Results.BadRequest();
+    //     await sender.Send(command);
+    //     return Results.NoContent();
+    // }
+    //
+    // public async Task<IResult> DeleteCompany(ISender sender, int id)
+    // {
+    //     await sender.Send(new DeleteCompanyCommand(id));
+    //     return Results.NoContent();
+    // }
+}
