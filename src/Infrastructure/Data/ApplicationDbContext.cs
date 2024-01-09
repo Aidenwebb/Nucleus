e51a5d593e2589124c620f01b772c4a48885f6d3
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2024-01-09 15:35:17 +0000

    Add Contacts to EfCore

--- src/Infrastructure/Data/ApplicationDbContext.cs
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ using Nucleus.Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Nucleus.Domain.CompanyAggregate.Entities;
+using Nucleus.Domain.ContactAggregate.Entities;
 
 namespace Nucleus.Infrastructure.Data;
 
@@ -17,6 +18,8 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IApplica
     public DbSet<TodoItem> TodoItems => Set<TodoItem>();
 
     public DbSet<Company> Companies => Set<Company>();
+    
+    public DbSet<Contact> Contacts => Set<Contact>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
---
+++ src/Infrastructure/Data/Migrations/20240109153410_Add Contact MVP.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nucleus.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddContactMVP : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyId = table.Column<int>(type: "int", nullable: false),
+                    GivenName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    FamilyName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    ManagerContactId = table.Column<int>(type: "int", nullable: true),
+                    AssistantContactId = table.Column<int>(type: "int", nullable: true),
+                    Created = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModified = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contacts_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Contacts_Contacts_AssistantContactId",
+                        column: x => x.AssistantContactId,
+                        principalTable: "Contacts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Contacts_Contacts_ManagerContactId",
+                        column: x => x.ManagerContactId,
+                        principalTable: "Contacts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_AssistantContactId",
+                table: "Contacts",
+                column: "AssistantContactId",
+                unique: true,
+                filter: "[AssistantContactId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_CompanyId",
+                table: "Contacts",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_ManagerContactId",
+                table: "Contacts",
+                column: "ManagerContactId",
+                unique: true,
+                filter: "[ManagerContactId] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
