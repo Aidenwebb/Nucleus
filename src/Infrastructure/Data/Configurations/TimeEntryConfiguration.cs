5b170bdf2dcba9de030369fb1a3e995a92840bcb
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2024-01-12 15:05:04 +0000

    Add TimeEntry to EfCore configuration

---
+++ src/Infrastructure/Data/Configurations/TimeEntryConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nucleus.Domain.TimeEntryAggregate.Entities;
+
+namespace Nucleus.Infrastructure.Data.Configurations;
+
+public class TimeEntryConfiguration : IEntityTypeConfiguration<TimeEntry>
+{
+    public void Configure(EntityTypeBuilder<TimeEntry> builder)
+    {
+        builder.Property(timeEntry => timeEntry.CompanyId)
+            .IsRequired();
+
+        builder.Property(timeEntry => timeEntry.TimeStart)
+            .HasDefaultValue(DateTime.UtcNow)
+            .IsRequired();
+    }
+}
